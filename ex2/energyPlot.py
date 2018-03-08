dd390091f42d28010cda99f2e32c5086b255baf1
Tim TY Lee <tyl35@cam.ac.uk>
2018-03-08 13:11:09 +0000

    added colour parameter to plot of energy difference

--- ex2/energyPlot.py
+++ ex2/energyPlot.py
@@ -28,7 +28,7 @@ ax1.set_ylim(np.array([0, 14]))
 ax1.tick_params('y', colors='#1f77b4')
 # plot of total energy against time (relative to starting energy)
 ax1.plot(tarr*10**(-3)/(2*np.pi),
-         (E_RK-E_RK[0])/E_RK[0]*10**(9), label="4th order R-K")
+         (E_RK-E_RK[0])/E_RK[0]*10**(9), label="4th order R-K", color='#1f77b4')
 ax2 = ax1.twinx()  # plot either result on separate axes of same graph (they differ by ~10^3)
 ax2.plot(tarr*10**(-3)/(2*np.pi),
          (E_LS-E_LS[0])/E_LS[0]*10**(6), label='LSODA', color='#ff7f0e')
